7b3b8470d562e920af62daa07d5e15c7f8f8f143
antoniofendy <layfenfen@gmail.com>
2022-03-26 21:49:31 +0700

    Register Backend 1

--- app/src/main/java/com/kelompok15/aplikasihitungbangunruang/DBController.java
+++ app/src/main/java/com/kelompok15/aplikasihitungbangunruang/DBController.java
@@ -1,7 +1,60 @@
 package com.kelompok15.aplikasihitungbangunruang;
 
-public class DBController {
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
 
+public class DBController extends SQLiteOpenHelper
+{
 
+    private static final String namaDatabase = "app_geometri.db";
+
+    public DBController(@Nullable Context context, @Nullable SQLiteDatabase.CursorFactory factory, int version)
+    {
+        super(context, namaDatabase, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase)
+    {
+        sqLiteDatabase.execSQL("CREATE TABLE users (" +
+                "user_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "username TEXT UNIQUE, " +
+                "password TEXT);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1)
+    {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS users;");
+        onCreate(sqLiteDatabase);
+    }
+
+    public void insertUser(User user)
+    {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("username", user.getUsername());
+        contentValues.put("password", user.getPassword());
+        this.getWritableDatabase().insert("users", "", contentValues);
+    }
+
+    public User auth(User user)
+    {
+        @SuppressLint("Recycle") Cursor cursor = this.getReadableDatabase().rawQuery(
+                "SELECT * FROM users WHERE username = ? AND password = ?", new String[]{user.getUsername(), user.getPassword()}
+        );
+
+        if(cursor != null && cursor.moveToFirst() && cursor.getCount() > 0)
+        {
+            User result = new User(cursor.getInt(0), cursor.getString(1), cursor.getString(2));
+            return result;
+        }
+
+        return null;
+    }
 }
--- app/src/main/java/com/kelompok15/aplikasihitungbangunruang/RegisterActivity.java
+++ app/src/main/java/com/kelompok15/aplikasihitungbangunruang/RegisterActivity.java
@@ -3,22 +3,59 @@ package com.kelompok15.aplikasihitungbangunruang;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity{
+public class RegisterActivity extends AppCompatActivity
+{
+    private EditText etUsername, etPassword, etPasswordRepeat;
+    private DBController dbController;
+    private String message;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etUsername = findViewById(R.id.edittext_username);
+        etPassword = findViewById(R.id.edittext_password);
+        etPasswordRepeat = findViewById(R.id.edittext_password_repeat);
+
+        dbController = new DBController(this, null, 1);
     }
 
     public void register(View view)
     {
-        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        try
+        {
+            if(validasiInput(etUsername.getText().toString(), "username"))
+            {
+                Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+            else if(validasiInput(etPassword.getText().toString(), "password"))
+            {
+                Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+            else if(validasiInput(etPasswordRepeat.getText().toString(), "ulangi password"))
+            {
+                Toast.makeText(RegisterActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+            else
+            {
+                dbController.insertUser(new User(null, etUsername.getText().toString(), etPassword.getText().toString()));
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+        catch (SQLiteException e)
+        {
+            Toast.makeText(RegisterActivity.this, "Username sudah digunakan pengguna lain", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void toLogin(View view)
@@ -28,4 +65,25 @@ public class RegisterActivity extends AppCompatActivity{
         finish();
     }
 
+    //Fungsi untuk validasi input
+    public boolean validasiInput (String inputValue, String inputType)
+    {
+        if(inputValue.length() == 0)
+        {
+            message = "Anda harus mengisi field " +  inputType + "!";
+            return true;
+        }
+        else if (inputValue.length() < 5)
+        {
+            message = "Field " + inputType + " membutuhkan minimum 5 karakter";
+            return true;
+        }
+        else if(inputType.equals("ulangi password") && inputValue.compareTo(etPassword.getText().toString()) != 0)
+        {
+            message = "Field " + inputType + " tidak sama dengan field password";
+            return true;
+        }
+
+        return false;
+    }
 }
