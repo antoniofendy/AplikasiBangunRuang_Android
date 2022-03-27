bc3f72f65aefeee74756019c0d65467cda079012
antoniofendy <layfenfen@gmail.com>
2022-03-27 20:45:15 +0700

    Cube Calculator

--- app/src/main/java/com/kelompok15/aplikasihitungbangunruang/CubeActivity.java
+++ app/src/main/java/com/kelompok15/aplikasihitungbangunruang/CubeActivity.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
@@ -16,6 +19,8 @@ import java.util.Objects;
 
 public class CubeActivity extends AppCompatActivity
 {
+    EditText etEdgeInput;
+    TextView tvSurfaceResult, tvVolumeResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,6 +28,10 @@ public class CubeActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cube);
         Objects.requireNonNull(getSupportActionBar()).hide();
+
+        etEdgeInput = findViewById(R.id.edittext_edge);
+        tvSurfaceResult = findViewById(R.id.textview_surface_result);
+        tvVolumeResult = findViewById(R.id.textview_volume_result);
 
         MaterialToolbar toolbar = (MaterialToolbar) findViewById(R.id.topAppBar);
 
@@ -51,6 +60,25 @@ public class CubeActivity extends AppCompatActivity
 
     public void calculate(View view)
     {
-        Toast.makeText(CubeActivity.this, "Ok", Toast.LENGTH_SHORT).show();
+        if(etEdgeInput.length() != 0)
+        {
+            double edge = Double.parseDouble(String.valueOf(etEdgeInput.getText()));
+            double surface = 6 * (Math.pow(edge, 2));
+            double volume = Math.pow(edge, 3);
+
+            tvSurfaceResult.setText(String.valueOf(surface));
+            tvVolumeResult.setText(String.valueOf(volume));
+        }
+        else
+        {
+            Toast.makeText(CubeActivity.this, "Anda belum menginput nilai sisi a", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void delete(View view)
+    {
+        etEdgeInput.setText("");
+        tvSurfaceResult.setText("-");
+        tvVolumeResult.setText("-");
     }
 }
