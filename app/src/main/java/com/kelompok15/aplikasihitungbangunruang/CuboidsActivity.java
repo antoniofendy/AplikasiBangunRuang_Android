dbef21b2c8caf8585166dcd4f7e923435a1cca4c
JoeShane11 <joeshane1103@gmail.com>
2022-03-27 21:46:50 +0700

    Create Activity Balok (Cuboids)
    Create Java, dan Layout File

---
+++ app/src/main/java/com/kelompok15/aplikasihitungbangunruang/CuboidsActivity.java
@@ -0,0 +1,15 @@
+package com.kelompok15.aplikasihitungbangunruang;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CuboidsActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuboids);
+    }
+
+}
